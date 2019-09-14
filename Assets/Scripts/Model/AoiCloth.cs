e9d4ebe90ed06795d0a74dcd11ac7dced8d932d8
Hisashi Teruya <vxd.naoshi.19961205.maro@gmail.com>
2019-09-14 20:19:19 +0900

    服を管理するスクリプト、ランダムにモーションをするスクリプトの作成

--- Assets/Scripts/Model/AoiCloth.cs
+++ Assets/Scripts/Model/AoiCloth.cs
@@ -27,7 +27,10 @@ public class AoiCloth : MonoBehaviour
 	[SerializeField]
 	private Texture m_yellow;
 
+	private Dictionary<GameManager.PantsColor, Texture> m_pants = new Dictionary<GameManager.PantsColor, Texture>();
+
 	private void Awake() {
+		InitializePants();
 		m_propID = Shader.PropertyToID("_MainTex");
 	}
 
@@ -35,13 +38,22 @@ public class AoiCloth : MonoBehaviour
 	void Update()
     {
 
+		if (Input.GetKeyDown(KeyCode.W))
+			SetPants(GameManager.PantsColor.White);
+		else if (Input.GetKeyDown(KeyCode.B))
+			SetPants(GameManager.PantsColor.Black);
+		else if (Input.GetKeyDown(KeyCode.P))
+			SetPants(GameManager.PantsColor.Purple);
+		else if (Input.GetKeyDown(KeyCode.Q))
+			SetPants(GameManager.PantsColor.Pink);
+
+
     }
 
 
-	public void SetPants() {
+	public void SetPants(GameManager.PantsColor pantsColor) {
 
-
-
+		SetPantsTexture(m_pants[pantsColor]);
 
 	}
 
@@ -50,5 +62,19 @@ public class AoiCloth : MonoBehaviour
 		m_material.SetTexture(m_propID, pantsTexture);
 
 	}
-	
+
+	private void InitializePants() {
+
+		m_pants.Add(GameManager.PantsColor.White, m_white);
+		m_pants.Add(GameManager.PantsColor.Black, m_black);
+		m_pants.Add(GameManager.PantsColor.Pink, m_pink);
+		m_pants.Add(GameManager.PantsColor.Red, m_red);
+		m_pants.Add(GameManager.PantsColor.Blue, m_blue);
+		m_pants.Add(GameManager.PantsColor.Grren, m_green);
+		m_pants.Add(GameManager.PantsColor.Purple, m_purple);
+		m_pants.Add(GameManager.PantsColor.Yellow, m_yellow);
+
+
+	}
+
 }
---
+++ Assets/Scripts/Model/RandomAnimatorSelecter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimatorSelecter : MonoBehaviour
+{
+
+	[SerializeField]
+	private List<RuntimeAnimatorController> m_AnimatorController;
+
+	private Animator m_animator;
+
+	private void Awake() {
+
+		InitializeAnimator();
+
+	}
+
+	private void Start() {
+
+	}
+
+	void InitializeAnimator() {
+
+		m_animator = GetComponent<Animator>();
+		m_animator.runtimeAnimatorController = m_AnimatorController[Random.Range(0, m_AnimatorController.Count)];
+
+	}
+
+}
