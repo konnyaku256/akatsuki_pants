44a35a3f3812c6dbb9ca358ba93a2893076a9fea
Hisashi Teruya <vxd.naoshi.19961205.maro@gmail.com>
2019-09-15 12:13:23 +0900

    頷くアニメーションの追加、AnimatorControllerの修正

--- Assets/Scripts/Model/AoiAniamtorManager.cs
+++ Assets/Scripts/Model/AoiAniamtorManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +9,29 @@ public class AoiAniamtorManager : MonoBehaviour
 	[SerializeField]
 	private int m_hintCount;
 
+	[SerializeField]
+	private float m_jump1Volume;
+	[SerializeField]
+	private float m_jump2Volume;
+	[SerializeField]
+	private float m_jump3Volume;
+
 	private Animator m_animator;
 
 	private int m_wrongCount;
 
-    // Start is called before the first frame update
-    void Start()
+	public Single Jump1Volume {
+		get {
+			return m_jump1Volume;
+		}
+
+		set {
+			m_jump1Volume = value;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
 
 		m_animator = GetComponent<Animator>();
@@ -40,10 +58,26 @@ public class AoiAniamtorManager : MonoBehaviour
 		if (stateinfor.IsName("Refuse1") || stateinfor.IsName("Refuse2"))
 			return;
 
-		if (Random.Range(0, 2) == 1)
+		if (UnityEngine.Random.Range(0, 2) == 1)
 			m_animator.SetTrigger("Refuse1");
 		else
 			m_animator.SetTrigger("Refuse2");
+
+	}
+
+	public void AoiJump(float voiceVolume) {
+
+		if (voiceVolume > m_jump3Volume)
+			m_animator.SetTrigger("Jump3");
+		else if (voiceVolume > m_jump2Volume)
+			m_animator.SetTrigger("Jump2");
+		else if (voiceVolume > m_jump1Volume)
+			m_animator.SetTrigger("Jump1");
+	}
+
+	public void AoiNodAnimation() {
+
+		m_animator.SetTrigger("Nod");
 
 	}
 
